fc08a87a2f246799cb475e91b0cf837584fe068b
Vatsal Bora <vatsalbora@Vatsals-MacBook-Air.local>
2023-02-16 22:15:26 -0500

    Changed home page

--- src/components/home.js
+++ src/components/home.js
@@ -5,15 +5,18 @@ import LogoutButton from './logout.js'
 
 function Home() {
     return (
-        <div className="page">
-            <h1>GiftGuru</h1>
-            <LogoutButton/>
-            <form>
-                <input name="Name" placeholder="Name" required/>
-            </form>
+        <div className="container">
+            <div className="text">
+                <h1>Hi there!</h1>
+                <LogoutButton/>
                 <Link to="/requirements" style={{ textDecoration: 'none', color: '#FFF' }}>
-                    <button className="submit">Submit</button>
+                    <button className="submit">Let's find a gift!</button>
+                
                 </Link>
+            </div>
+            <div className="image">
+                <img src={require("../images/gift.png")} alt="Gift"/>
+            </div>
         </div> 
     );
 }
