97390c96ac9021d82570eb6afe867fa4bd7ffeac
ewnagteg <ewnagteg@users.noreply.github.com>
2023-03-01 00:25:02 -0500

    fix selection to use pinid

--- src/components/choices.js
+++ src/components/choices.js
@@ -9,37 +9,34 @@ const ImageGrid = () => {
     state: ''
   };
 
-  const baseUrl = 'https://duhynhz8i1.loclx.io';
   var bodyFormData = new FormData();
   bodyFormData.append('state', '');
-  const [selectedImages, setSelectedImages] = useState([]);
+  const [selectedImages, setSelectedImages] = useState({});
   const [isLoading, setLoading] = useState(true);
   const [pins, setPins] = useState();
 
   useEffect(() => {
     axios.put("/seed").then(response => {
         let images = [];
-        console.log('respose');
-        console.log(response.data);
         for (let i in response.data['pins']) {
           let image = response.data ['pins'][i];
           image['id'] = i;
           images.push(image);
         }
-        console.log(images);
         setPins(images);
         setLoading(false);
     }, bodyFormData);
   }, []);
 
-
   const checkmark = require("../images/checkmark.png");
 
-  const handleImageClick = (index) => {
-    if (selectedImages.includes(index)) {
-      setSelectedImages(selectedImages.filter(i => i !== index));
+  const handleImageClick = (image) => {
+    if (!(image['id'] in selectedImages)) {
+      setSelectedImages(selectedImages => ({...selectedImages, [image['id']]: image}));
     } else {
-      setSelectedImages([...selectedImages, index]);
+      let copy = {...selectedImages};
+      delete copy[image['id']];
+      setSelectedImages(selectedImages => (copy));
     }
     console.log(selectedImages);
   };
@@ -58,15 +55,15 @@ const ImageGrid = () => {
         {pins.map((image, index) => (
           <div key={image['id']}>
             <img src={image['img']} alt = 'Gift'
-            className={`image-wrapper ${selectedImages.includes(index) ? 'selected' : ''}`}
-            onClick={() => handleImageClick(index)} />
-            {selectedImages.includes(index) && (<img src={checkmark} alt="" className="checkmark"/>)}
+            className={`image-wrapper ${( image['id'] in selectedImages ) ? 'selected' : ''}`}
+            onClick={() => handleImageClick(image)} />
+            {( image['id'] in selectedImages ) && (<img src={checkmark} alt="" className="checkmark"/>)}
           </div>
         ))}
       </div>
       <Link to="/recommendations" style={{ textDecoration: 'none', color: '#FFF' }}>
         <button className="submit">Submit</button>
-        </Link>
+      </Link>
     </div>
   );
 
