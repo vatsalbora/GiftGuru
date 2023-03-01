9cd2af258278f6298c43a5c9d97711d1f4f8c2cd
Suthan <suthan.sivekumaar@gmail.com>
2023-02-28 19:36:52 -0500

    Initial changes

--- src/components/choices.js
+++ src/components/choices.js
@@ -2,27 +2,34 @@ import React, {useState} from "react";
 import './styles.css';
 import LogoutButton from './logout.js';
 import { Link } from 'react-router-dom'
-
+import axios from 'axios';
 const ImageGrid = () => {
-  const images = [
-    require("../images/options/image1.png"),
-    require("../images/options/image2.png"),
-    require("../images/options/image3.png"),
-    require("../images/options/image4.png"),
-    require("../images/options/image5.png"),
-    require("../images/options/image6.png"),
-    require("../images/options/image7.png"),
-    require("../images/options/image8.png"),
-    require("../images/options/image9.png"),
-    require("../images/options/image10.png")
-  ];
+  const data = {
+    state: ''
+  };
+const baseUrl = 'https://duhynhz8i1.loclx.io';
+const [selectedImages, setSelectedImages] = useState([]);
 
-  const [selectedImages, setSelectedImages] = useState([]);
-  
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   // handle file submission here
-  // }
+const updateData = async (data) => {
+  try {
+    const response = await axios.put(`${baseUrl}/seed`, data, {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    });  
+    console.log(response.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+let response = updateData(data);
+let images = [];
+
+for (let i in response['pins']) {
+  let image = response['pins'][i];
+  image['id'] = i;
+  images.push(image);
+}
 
   const checkmark = require("../images/checkmark.png");
 
@@ -44,8 +51,8 @@ const ImageGrid = () => {
       </Link>
       <div className="image-grid">
         {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} alt = 'Gift'
+          <div key={image['id']}>
+            <img src={image['img']} alt = 'Gift'
             className={`image-wrapper ${selectedImages.includes(index) ? 'selected' : ''}`}
             onClick={() => handleImageClick(index)} />
             {selectedImages.includes(index) && (<img src={checkmark} alt="" className="checkmark"/>)}
