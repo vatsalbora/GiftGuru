010262acb820085aeeac7afe4d1cc98b808b8f7a
Vatsal Bora <vatsalbora@v1040-wn-rt-a-76-60.campus-dynamic.uwaterloo.ca>
2023-03-01 17:56:19 -0500

    Minor changes completed

--- src/components/requirements.js
+++ src/components/requirements.js
@@ -12,18 +12,30 @@ function Requirements() {
           <button className="back">Back</button>
         </Link>
         <form>
-            <input name="name" placeholder="Recipient's Name" required />
-            <input name="age" placeholder="Recipient's Age" required />
-            <select name="gender" required>
-              <option value="" disabled selected>Recipient's Gender</option>
-              <option value="Female">Woman</option>
-              <option value="Male">Man</option>
-              <option value="Transgender">Transgender</option>
-              <option value="Non-Binary/Non-Conforming">Non-Binary/Non-Conforming</option>
-              <option value="Prefer Not To Say">Prefer Not To Say</option>
+            <input name="name" placeholder="Waht is the recipient's name?" required />
+            <select name="age" required>
+              <option value="" disabled selected>What is the recipient's age group?</option>
+              <option value="10">&lt; 10 years</option>
+              <option value="18">10-18 years</option>
+              <option value="35">19-35 years</option>
+              <option value="50">36-50 years</option>
+              <option value="">&gt; 50 years</option>
               </select>
-            <input name="budget" placeholder="What is your budget?" required />
-            <input name="pinterest" placeholder="Recipient's Pinterest profile link (if applicable)" />
+            <select name="gender" required>
+              <option value="" disabled selected>Preferred gift theme for the recipient</option>
+              <option value="Female">Feminine</option>
+              <option value="Male">Masculine</option>
+              <option value="">Gender-Neutral</option>
+              </select>
+            <select name="budget" required>
+              <option value="" disabled selected>What is your budget? (CAD)</option>
+              <option value="50">&lt; $50</option>
+              <option value="100">$50-$100</option>
+              <option value="200">$100-$200</option>
+              <option value="500">$200-$500</option>
+              <option value="">&gt; $500</option>
+              </select>
+            <input name="pinterest" placeholder="Recipient's Pinterest username (if applicable)" />
         </form>
         <Link to="/choices" style={{ textDecoration: 'none', color: '#FFF' }}>
           <button className="submit">Submit</button>
